46e2da9bc378980e78b42760669688906d1a1a4e
mistifikatorcat <92595353+mistifikatorcat@users.noreply.github.com>
2022-10-30 23:33:18 +0200

    debug +fix some comments

--- backend/routes/notARoute.js
+++ backend/routes/notARoute.js
@@ -1,10 +1,9 @@
-const router = require('express').Router();
 const NotFound = require('../errors/NotFound');
 
-router.use('*',(res, req, next) => {
+const notARoute = (res, req, next) => {
   next(new NotFound('Page does not exist'));
-})
+};
 
 module.exports = {
-  notARoute: router
+  notARoute
 };
