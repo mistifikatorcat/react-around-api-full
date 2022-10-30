416996c87e4b7aeab5d251fa26ee17d052eb89a8
mistifikatorcat <92595353+mistifikatorcat@users.noreply.github.com>
2022-10-30 23:17:57 +0200

    debug +fix some comments

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
 const {validateProfile, validateProfilePic, validateObjectId} = require('../middleware/validation');
 const {
-  getUser, getCurrentUser, getAllUsers, updateProfile, updateProfilePicture,
+  getUser, getCurrentUser, getAllUsers, updateProfile, updateProfilePicture, createUser
 } = require('../controllers/users');
 //const auth = require('../middleware/auth');
 
 
 router.get('/', getAllUsers);
 router.get('/me', getCurrentUser);
-router.get('/:userId', validateObjectId, getUser);
-//router.post('/users', createUser);
+//router.get('/:userId', validateObjectId, getUser);
+router.post('/users', createUser);
 router.patch('/me',  validateProfile, updateProfile);
 router.patch('/me/avatar', validateProfilePic, updateProfilePicture);
 
